e2c3c953cc47602e451df9eaca2b2faeb2828fe7
trueeric <ikpassat@gmail.com>
2025-08-27 05:52:41 +0800

    add netlify app url

--- src/config/api.js
+++ src/config/api.js
@@ -3,4 +3,5 @@ export const API_CONFIG = {
   GOOGLE_SCRIPT_URL:
     'https://script.google.com/macros/s/AKfycbzMOPb6jfQqpGeiZbZrt78_bC1aMYKzlgiJ-tB1PNUDthpVEDgckqDcNc1X7TnamFpxrA/exec',
   API_BASE_URL: 'https://your-api-url.com',
+  NETLIFY_APP_URL: 'https://your-new-netlify-app-url.netlify.app',
 }
